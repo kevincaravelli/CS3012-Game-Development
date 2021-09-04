41201e1bb3e3845cc48b1f87609005a6d76067da
Alpha Centauri <kcaravelli79@gmail.com>
2021-09-04 18:24:14 -0400

    Finish Assignment #2
    Finished everything with Assignment 2. Created a script to rotate the square as well as buttons linked to increase or decrease the speed at which it rotates.

--- Assignment #2/Assets/Scripts/NewBehaviourScript.cs
+++ Assignment #2/Assets/Scripts/NewBehaviourScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    [SerializeField] GameObject mySquare;
+    float RotationSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotates the square counter clockwise
+        mySquare.transform.Rotate(Vector3.forward * RotationSpeed * Time.deltaTime);
+    }
+
+    public void updateRotationVelocity(float speed)
+    {
+        RotationSpeed += speed;
     }
 }
