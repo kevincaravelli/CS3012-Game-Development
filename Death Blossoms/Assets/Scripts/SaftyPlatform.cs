6db3e56642c701ba727d4ea4b831f9fd64977b38
AlphaCentauri79 <kcaravelli79@gmail.com>
2021-10-27 15:04:34 -0400

    Attempt to fix Saftey Platform
    I attempted to fix the saftey platform. I made a boolean that will detect if the player is moving closer to the platform then it will raise its self.
    
    BUG: Safety Platform starts the game at ground level, when it should start at -7.5.

--- Death Blossoms/Assets/Scripts/SaftyPlatform.cs
+++ Death Blossoms/Assets/Scripts/SaftyPlatform.cs
@@ -4,32 +4,59 @@ using UnityEngine;
 
 public class SaftyPlatform : MonoBehaviour
 {
-    [SerializeField] float velocity = 10.0f;
+    [SerializeField] float velocity = 1.0f;
     [SerializeField] GameObject player;
+    private float distance;
+    private bool closer = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkDistance();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkDistance();
+        movePlatform();
     }
 
     private void movePlatform()
     {
-        // Current position of platform
         var position = transform.position;
-        var playerPos = player.transform.position;
 
-        // We only want the platform as the player gets closer
-        // It has to be at the ground level by the time they arrive
-        // Update the y component
-        position.y += playerPos.x * Time.deltaTime * velocity;
+        if (closer) // Platform goes up if closer is true
+        {
+            position.y = Time.deltaTime * velocity;
+        } else
+        {
+            position.y = Time.deltaTime * -velocity;
+        }
+
+        // Constrain the saftey platform inbetween -1.5 and -7.5
+        if (position.y >= -1.5) position.y = -1.5f;
+        if (position.y <= -7.5) position.y = -7.5f;
 
         transform.position = position;
     }
+
+    private void checkDistance()
+    {
+        var playerPos = player.transform.position;
+        var position = transform.position;
+        var checkDist = Vector2.Distance(position, playerPos);
+
+        // Check if the distance is less then it was (player getting closer)
+        if ( checkDist < distance)
+        {
+            closer = true;
+        } else
+        {
+            closer = false;
+        }
+
+        distance = checkDist;
+    }
+
 }
